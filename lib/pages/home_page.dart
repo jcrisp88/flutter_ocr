4b15e1bde65b2f245bca8b0baf2c4e75ced027e5
hiwa-jalal <hiwadoski21@gmail.com>
2020-09-01 23:03:23 +0300

    recognize company names

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -51,8 +51,8 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<List<String>> _readNamesFile() async {
-    fileText = await rootBundle.loadString('assets/files/names.txt');
+  Future<List<String>> _readNamesFile(String fileName) async {
+    fileText = await rootBundle.loadString('assets/files/$fileName.txt');
     return LineSplitter().convert(fileText);
   }
 
@@ -60,53 +60,59 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _isImageLoaded
-                  ? Center(
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: FileImage(_pickedImage),
-                            fit: BoxFit.cover,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _isImageLoaded
+                    ? Center(
+                        child: Container(
+                          height: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(_pickedImage),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  : Container(),
-              Text(
-                imageText.toString(),
-                style: TextStyle(fontSize: 26),
-              ),
-              RaisedButton(
-                child: Text('Pick an image'),
-                onPressed: _pickImage,
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                child: Text('Read Image Text'),
-                onPressed: _readImageText,
-              ),
-              RaisedButton(
-                child: Text('Read From file'),
-                onPressed: _readNamesFile,
-              ),
-              SizedBox(height: 10),
-              _buildTypeText(),
-              _buildProductText(),
-              Text(
-                'BB:${imageText.where((element) => Constants.bbe.indexOf(element) >= 0)}',
-                style: TextStyle(fontSize: 26),
-              ),
-              _buildWeightText(),
-            ],
+                      )
+                    : Container(),
+                Text(
+                  imageText.toString(),
+                  style: TextStyle(fontSize: 26),
+                ),
+                RaisedButton(
+                  child: Text('Pick an image'),
+                  onPressed: _pickImage,
+                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  child: Text('Read Image Text'),
+                  onPressed: _readImageText,
+                ),
+                // RaisedButton(
+                //   child: Text('Read From file'),
+                //   onPressed: () => _readNamesFile('names'),
+                // ),
+                SizedBox(height: 10),
+                _buildTypeText(),
+                _buildProductText(),
+                _buildWeightText(),
+                _buildCompanyText(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Text _buildBbeText() {
+    return Text(
+      'BBE:',
+      style: TextStyle(fontSize: 20),
     );
   }
 
@@ -114,33 +120,62 @@ class _HomePageState extends State<HomePage> {
     List<String> typeText = imageText
         .where((element) => Constants.types.indexOf(element.toLowerCase()) >= 0)
         .toList();
+    if (typeText.isNotEmpty) {
+      return Text(
+        'Type: ${typeText[0]}',
+        style: TextStyle(fontSize: 20),
+      );
+    }
     return Text(
-      'Type: ${typeText.elementAtOrElse(0, () => '').toLowerCase()}',
+      'Type:',
       style: TextStyle(fontSize: 20),
     );
   }
 
-  Widget _buildProductText() {
-    String productName = '';
-
+  Widget _buildCompanyText() {
+    var companyName = '';
     return FutureBuilder<List<String>>(
-      future: _readNamesFile(),
+      future: _readNamesFile('company_names'),
       builder: (context, fileTextSnapshot) {
         if (fileTextSnapshot.hasData) {
           for (var word in fileTextSnapshot.data) {
             final regex = RegExp(word, caseSensitive: false);
-            var iter = regex.allMatches(visionText?.text);
-            for (var element in iter) {
-              productName =
-                  visionText.text.substring(element.start, element.end);
+            if (visionText != null) {
+              var regexMatches = regex.allMatches(visionText.text);
+              for (var element in regexMatches) {
+                companyName =
+                    visionText.text.substring(element.start, element.end);
+              }
             }
           }
-          // final loweredCaseFileList =
-          //     fileTextSnapshot.data.map((e) => e.toLowerCase()).toList();
-          // final matchedList = imageText
-          //     .where((element) =>
-          //         loweredCaseFileList.indexOf(element.toLowerCase()) >= 0)
-          // .toList();
+          return Text(
+            'Company: ${companyName.toLowerCase()}',
+            style: TextStyle(fontSize: 20),
+          );
+        }
+        return Text('Company:', style: TextStyle(fontSize: 20));
+      },
+    );
+  }
+
+  Widget _buildProductText() {
+    var productName = ' ';
+
+    return FutureBuilder<List<String>>(
+      future: _readNamesFile('names'),
+      builder: (context, fileTextSnapshot) {
+        if (fileTextSnapshot.hasData) {
+          for (var word in fileTextSnapshot.data) {
+            final regex = RegExp(word, caseSensitive: false);
+            if (visionText != null) {
+              var iter = regex.allMatches(visionText.text);
+              for (var element in iter) {
+                productName =
+                    visionText.text.substring(element.start, element.end);
+              }
+            }
+          }
+
           return Text(
             'Product Name: ${productName.toLowerCase()}',
             style: TextStyle(fontSize: 20),
@@ -154,17 +189,22 @@ class _HomePageState extends State<HomePage> {
   Text _buildWeightText() {
     String visionString;
     String weight;
-    final regex = RegExp(r'\d.+?(?:g|kg)');
+    final regex = RegExp(r'([0-9]+[.,]*)+(?:g|kg)');
     if (visionText != null) {
-      visionString = visionText.text;
-      final iter = regex.allMatches(visionString);
-      for (var element in iter) {
+      visionString = visionText.text.replaceAll(RegExp(r"\s+\b|\b\s"), '');
+      final regexMatches = regex.allMatches(visionString);
+      for (var element in regexMatches) {
         weight = visionString.substring(element.start, element.end);
       }
     }
-
+    if (weight != null) {
+      return Text(
+        'Weight: $weight',
+        style: TextStyle(fontSize: 20),
+      );
+    }
     return Text(
-      'Weight: $weight',
+      'Weight: ',
       style: TextStyle(fontSize: 20),
     );
   }
